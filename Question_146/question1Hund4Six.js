16bd81cb101684607e97df266046706920a30314
silentshaikh <sheikhabdulmoiz283@gmail.com>
2024-04-21 15:35:38 +0500

    Add Files

---
+++ Question_146/question1Hund4Six.js
@@ -0,0 +1,5 @@
+setTimeout(function () {
+    console.log('Hello World');
+}, 3000);
+console.log('Task Completed');
+// It is commonly used in web development for various purposes, such as  implementing timeouts for asynchronous operations, and scheduling tasks to be executed in the future.
